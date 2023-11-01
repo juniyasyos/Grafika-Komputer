fdb46fe79eb9ea5334cb3ef249ea524c0b55fd43
Juniyasyos <juniyasyos@gmail.com>
2023-11-01 12:12:14 +0700

    ada yg ketinggalan (hehe)

--- Orion/graphics/gameplay/battle.py
+++ Orion/graphics/gameplay/battle.py
@@ -1,5 +1,6 @@
 import pygame
 from .player import Player
+from .enemy import obj_Enemy
 from icecream import ic
 import os
 import random
@@ -14,34 +15,6 @@ def load_image(image_filename):
     except pygame.error as e:
         print(f"Failed to load image: {image_filename}")
         raise e
-
-class obj_Enemy:
-    def __init__(self, x, y, image, screen):
-        self.x = x
-        self.y = y
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
-        self.screen = screen
-        self.health = 150
-        self.max_health = 150
-        self.last_BasicAttack_time = 0
-        self.damage = 50
-        self.basic_attack_path = pygame.transform.rotate(load_image("../resources/assets/Battle/bullet.png"), 180)
-
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
-    
-    def draw_health(self):
-        pygame.draw.rect(self.screen, (255, 0, 0), (self.rect.x, self.rect.y - 10, self.rect.width, 5))
-        current_health_width = (self.health / self.max_health) * self.rect.width
-        pygame.draw.rect(self.screen, (0, 255, 0), (self.rect.x, self.rect.y - 10, current_health_width, 5))
-    
-    def create_basic_attack_enemy(self, BasicAttack, enemy, amount_BasicAttack, current_time, last_time, cooldown_basicAttack):
-        if current_time - last_time >= cooldown_basicAttack:
-            return BasicAttack(enemy, damage=10, speed=15, image=pygame.transform.scale(self.basic_attack_path, (30, 30)), direction="down")
-        return None
-
 
 class BasicAttack:
     def __init__(self, actor, damage, speed, image, direction):
@@ -145,7 +118,7 @@ class Battle:
             for enemy in self.Enemys:
                 for attack in self.enemy_basic_attacks:
                     # Logika player kalah
-                    
+
                     # if self.player.health <= 0:
                     #     ic("player kalah")
                     if attack.rect.colliderect(self.player.rect):
--- Orion/graphics/gameplay/enemy.py
+++ Orion/graphics/gameplay/enemy.py
@@ -1,95 +1,42 @@
 import pygame
-import random
+from icecream import ic
 import os
+import random
 
 def load_image(image_filename):
     current_directory = os.path.dirname(os.path.abspath(__file__))  # Mengambil direktori saat ini
     image_path = os.path.join(current_directory, image_filename)  # Membuat jalur lengkap ke file gambar
-    
+
     try:
         image = pygame.image.load(image_path).convert_alpha()
         return image
     except pygame.error as e:
         print(f"Failed to load image: {image_filename}")
         raise e
-
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self, image, health, damage, speed, screen_width, screen_height):
-        super().__init__()
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.health = health
-        self.damage = damage
-        self.speed = speed
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-
-    def move(self):
-        pass
-
-    def update(self):
-        pass
-
-class KamikazeEnemy(Enemy):
-    def __init__(self, screen_width, screen_height):
-        image = pygame.transform.scale(load_image("../resources/assets/Battle/NPC.png"), (70, 70))
-        super().__init__(image, health=10, damage=5, speed=8, screen_width=screen_width, screen_height=screen_height)
-
-    def move(self):
-        # Implementasi pergerakan kamikaze enemy
-        pass
-
-    def update(self):
-        # Implementasi pembaruan kamikaze enemy
-        pass
-
-class ShooterEnemy(Enemy):
-    def __init__(self, screen_width, screen_height, x, y,):
-        image = pygame.transform.scale(load_image("../resources/assets/Battle/NPC.png"), (70, 70))
-        super().__init__(image, health=20, damage=10, speed=5, screen_width=screen_width, screen_height=screen_height)
-        self.basic_attack_path = load_image("../resources/assets/Battle/bullet.png")
+        
+class obj_Enemy:
+    def __init__(self, x, y, image, screen):
         self.x = x
         self.y = y
-        self.basic_attack_cooldown = 500
-        self.last_basicAttack_time = 0
+        self.image = image
         self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.screen = screen
+        self.health = 150
+        self.max_health = 150
+        self.last_BasicAttack_time = 0
+        self.damage = 50
+        self.basic_attack_path = pygame.transform.rotate(load_image("../resources/assets/Battle/bullet.png"), 180)
 
-    def move(self, lvl=None, speed=2):
-        self.rect.y += self.speed
-
-    def update(self):
-        self.move()
-
-
-    def create_basic_attack_enemy(self, BasicAttack, player, amount_BasicAttack, current_time, last_time):
-        last_time = 0  # Atur nilai awal yang sesuai untuk last_time
-        if amount_BasicAttack <= 4 and current_time - last_time >= 30:
-            return BasicAttack(player, damage=10, speed=20, image=pygame.transform.scale(self.basic_attack_path, (30, 30)), direction="down")
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+    
+    def draw_health(self):
+        pygame.draw.rect(self.screen, (255, 0, 0), (self.rect.x, self.rect.y - 10, self.rect.width, 5))
+        current_health_width = (self.health / self.max_health) * self.rect.width
+        pygame.draw.rect(self.screen, (0, 255, 0), (self.rect.x, self.rect.y - 10, current_health_width, 5))
+    
+    def create_basic_attack_enemy(self, BasicAttack, enemy, amount_BasicAttack, current_time, last_time, cooldown_basicAttack):
+        if current_time - last_time >= cooldown_basicAttack:
+            return BasicAttack(enemy, damage=10, speed=15, image=pygame.transform.scale(self.basic_attack_path, (30, 30)), direction="down")
         return None
-
-
-class StageBoss(Enemy):
-    def __init__(self, screen_width, screen_height):
-        image = pygame.transform.scale(load_image("../resources/assets/Battle/BOSS.png"), (70, 70))
-        super().__init__(image, health=100, damage=15, speed=3, screen_width=screen_width, screen_height=screen_height)
-
-    def move(self):
-        # Implementasi pergerakan bos tahap
-        pass
-
-    def update(self):
-        # Implementasi pembaruan bos tahap
-        pass
-
-class MainBoss(Enemy):
-    def __init__(self, screen_width, screen_height):
-        image = pygame.transform.scale(load_image("../resources/assets/Battle/BOSS.png"), (70, 70))
-        super().__init__(image, health=500, damage=25, speed=2, screen_width=screen_width, screen_height=screen_height)
-
-    def move(self):
-        # Implementasi pergerakan bos utama
-        pass
-
-    def update(self):
-        # Implementasi pembaruan bos utama
-        pass
